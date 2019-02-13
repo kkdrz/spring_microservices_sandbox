e947fa9f52c317ab18b384a4124345c3a9458374
Konrad Drozd <drozdkonrad@gmail.com>
2019-02-13 22:30:21 +0100

    Generate hello_world_microservice skeleton

---
+++ hello_world_microservice/src/main/java/com/drozd/hello_world_microservice/HelloWorldMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.drozd.hello_world_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldMicroserviceApplication.class, args);
+    }
+
+}
+
