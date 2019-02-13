70c6321a00846e7f6539e2de8c9c9eea025d9ef2
Konrad Drozd <drozdkonrad@gmail.com>
2019-02-14 00:12:18 +0100

    Make hello_world_microservice downloading configuration from config service

---
+++ hello_world_microservice/src/main/java/com/drozd/hello_world_microservice/HelloWorldEntity.java
@@ -0,0 +1,16 @@
+package com.drozd.hello_world_microservice;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class HelloWorldEntity {
+
+    @Id
+    @GeneratedValue
+    long id;
+
+    String someText;
+
+}
--- hello_world_microservice/src/main/java/com/drozd/hello_world_microservice/HelloWorldMicroserviceApplication.java
+++ hello_world_microservice/src/main/java/com/drozd/hello_world_microservice/HelloWorldMicroserviceApplication.java
@@ -2,7 +2,9 @@ package com.drozd.hello_world_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @SpringBootApplication
 public class HelloWorldMicroserviceApplication {
 
